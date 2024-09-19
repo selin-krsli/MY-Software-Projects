3eb77bbab38e4ed31ab76ee71e7571e26a68b005
Selin Karslı <selin.krsli@gmail.com>
2024-09-19 21:46:54 +0300

    Adding SkillController

---
+++ PortfolioProjectNight/Controllers/SkillController.cs
@@ -0,0 +1,50 @@
+﻿using PortfolioProjectNight.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioProjectNight.Controllers
+{
+    public class SkillController : Controller
+    {
+        DbMyPortfolioNightEntities1 context = new DbMyPortfolioNightEntities1();
+        public ActionResult SkillList()
+        {
+            var values = context.Skill.ToList();
+            return View(values);
+        }
+        [HttpGet]//attribute
+        public ActionResult CreateSkill()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateSkill(Skill skill)//parametre
+        {
+            context.Skill.Add(skill);
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+        public ActionResult DeleteSkill(int id)
+        {
+            var value = context.Skill.Find(id);
+            context.Skill.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+        [HttpGet]
+        public ActionResult UpdateSkill(int id)
+        {
+            var value = context.Skill.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateSkill(Skill skill)
+        {
+            
+            return View();
+        }
+    }
+}
